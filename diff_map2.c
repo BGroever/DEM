daea783c9af46e1e653d2c862bb8cb0137e5a446
ben <groever@g.harvard.edu>
2019-04-06 09:37:29 -0400

    Updating names

--- diff_map2.c
+++ diff_map2.c
@@ -1,9 +1,9 @@
 /*
 CS205 project:    Density equalizing map projections
-Date:             April 5th 2019
+Date:             April 6th 2019
 
 Compiler:         gcc diff_map2.c -o exec -lm -lpng
-project members:  Millie
+project members:  Millie Zhou, Lemaire Baptiste, Benedikt Groever
 project goal:     density equalizing map projections
 Input files:      -colchart.txt
                   -density.txt
