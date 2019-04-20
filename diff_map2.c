d1578a02ed58a4b030c4048c7f8067ab8e7969da
BGroever <35929506+BGroever@users.noreply.github.com>
2019-04-20 10:25:59 -0400

    Add files via upload

--- diff_map2.c
+++ diff_map2.c
@@ -28,6 +28,7 @@ static int getStringIndex(char (*c)[256], char *na)
           return j;
         }
     }
+    return 0;
 }
 
 //Find index of el in int array c
@@ -110,7 +111,7 @@ void read_png_file(char *filename)
 //Writer of png file
 void write_png_file(char *filename)
 {
-    int y;
+    //int y;
 
     FILE *fp = fopen(filename, "wb");
     if(!fp) abort();
@@ -153,6 +154,15 @@ void write_png_file(char *filename)
     fclose(fp);
 }
 
+<<<<<<< HEAD
+/* Function to integrate the density and reference
+map fields forward in time by dt */
+
+void step(double dt, double *time, double *u, double *cu, double *X, double *cX, double h, double ih2) {
+
+    double nu = dt/(h*h);
+    double fac = ih2*dt/h;
+=======
 
 
 
@@ -170,10 +180,85 @@ void step(double dt, double* time, double* u, double* cu, double* X, double* cX,
     double nu = dt/(h*h);
     double fac = ih2*dt/h;
     double maxvsq = 0.0;
+>>>>>>> cf7fbfddaad2973a2a77eb47d00905ab16315722
     double vx = 0;
     double vy = 0;
 
     /* Calculate the upwinded update for the reference map */
+<<<<<<< HEAD
+    for(int i=0; i < height; i++){
+      for(int j=0; j < width; j++){
+
+        if ((i>0) && (i<height-1)) {
+          vx = (-1.0) * (u[(i+1)*width+j]-u[(i-1)*width+j]) * fac / u[i*width+j];
+          if (vx > 0) {
+            cX[i*width*2+j*2+0] = vx*(-1*X[i*width*2+j*2+0] + X[(i-1)*width*2+j*2+0]);
+            cX[i*width*2+j*2+1] = vx*(-1*X[i*width*2+j*2+1] + X[(i-1)*width*2+j*2+1]);
+          }else{
+            cX[i*width*2+j*2+0] = vx*(   X[i*width*2+j*2+0] - X[(i+1)*width*2+j*2+0]);
+            cX[i*width*2+j*2+1] = vx*(   X[i*width*2+j*2+1] - X[(i+1)*width*2+j*2+1]);
+          }
+        }else{
+            cX[i*width*2+j*2+0] = 0.0;
+            cX[i*width*2+j*2+1] = 0.0;
+        }
+
+
+        if ( (j>0) && (j<width-1)) {
+          vy = (-1.0) * (u[i*width+(j+1)]-u[i*width+(j-1)]) * fac / u[i*width+j];
+          if (vy > 0) {
+            cX[i*width*2+j*2+0] += vy*(-1*X[i*width*2+j*2+0]+X[i*width*2+(j-1)*2+0]);
+            cX[i*width*2+j*2+1] += vy*(-1*X[i*width*2+j*2+1]+X[i*width*2+(j-1)*2+1]);
+          } else {
+            cX[i*width*2+j*2+0] += vy*(X[i*width*2+j*2+0]-1*X[i*width*2+(j+1)*2+0]);
+            cX[i*width*2+j*2+1] += vy*(X[i*width*2+j*2+1]-1*X[i*width*2+(j+1)*2+1]);
+          }
+        }
+      }
+    }
+
+    for(int i=0; i < height; i++){
+      for(int j=0; j < width; j++){
+        X[i*width*2+j*2+0] += cX[i*width*2+j*2+0];
+        X[i*width*2+j*2+1] += cX[i*width*2+j*2+1];
+      }
+    }
+
+    //int i = 50;
+    //int j = 150;
+    //vx = (-1.0) * (u[(i+1)*width+j]-u[(i-1)*width+j]) * fac / u[i*width+j];
+    //vy = (-1.0) * (u[i*width+(j+1)]-u[i*width+(j-1)]) * fac / u[i*width+j];
+    //printf("vx: %e for %d, vy: %e for %d\n", vx, i, vy, j);
+    //printf("x:  %e, y: %e\n", cX[i*width*2+j*2+0], cX[i*width*2+j*2+1]);
+
+    /* Do the finite-difference update */
+    double tem;
+    int k;
+    for (int i=0; i<height; i++) {
+        for (int j=0; j<width; j++) {
+            if (i>0){
+                tem = u[(i-1)*width+j]; k = 1;
+            }else{
+                tem = 0; k = 0;
+            }
+            if (j>0){
+                tem += u[i*width+(j-1)]; k += 1;
+            }
+            if (j<width-1){
+                tem += u[i*width+(j+1)]; k += 1;
+            }
+            if (i<height-1){
+                tem += u[(i+1)*width+j]; k += 1;
+            }
+            cu[i*width+j] = tem - k * u[i*width+j];
+        }
+    }
+
+    for(int i=0; i < height; i++){
+      for(int j=0; j < width; j++){
+        u[i*width+j] += cu[i*width+j] * nu;
+      }
+=======
     for (int i=0; i<width; i++) {
         for (int j=0; j<height; j++) {
             if ((i>0) && (i<width-1)) {
@@ -252,10 +337,34 @@ void step(double dt, double* time, double* u, double* cu, double* X, double* cX,
             temp2[i][j] = *(cu+i*height+j) * nu;
             *(u+i*height+j) += temp2[i][j];
         }
+>>>>>>> cf7fbfddaad2973a2a77eb47d00905ab16315722
     }
 
     /* Print the current time and the extremal values of density */
     *time += dt;
+<<<<<<< HEAD
+    double minU=16777215;
+    for (int i=0; i<height; i++) {
+      for (int j=0; j<width; j++) {
+        if (u[i*width+j] < minU) {
+          minU = u[i*width+j];
+        }
+      }
+    }
+
+    double maxU=0.0;
+    for (int i=0; i<height; i++) {
+      for (int j=0; j<width; j++) {
+        if (u[i*width+j] > maxU) {
+          maxU = u[i*width+j];
+        }
+      }
+    }
+
+    printf("%f, %f, %f \n", *time, minU, maxU);
+}
+
+=======
 
     double minU=0.0;
     for (int i=0; i<width; i++) {
@@ -286,6 +395,7 @@ void step(double dt, double* time, double* u, double* cu, double* X, double* cX,
 
 
 
+>>>>>>> cf7fbfddaad2973a2a77eb47d00905ab16315722
 // main program for density equalizing map projections
 int main(void)
 {
@@ -367,23 +477,23 @@ int main(void)
     c  - string array for the name of the states
     d  - unique bar code for each state (RGB) = R+256*G+65536*B
     rh[k] - population density of state c[k] */
+    /*
     for(int j=0; j<SIZE; j++){
       printf("%s, %d, %f\n", c[j], d[j], rh[j]);
-    }
+    }*/
 
     /* Read in the undeformed US map */
-    read_png_file("usa_vs.png");
+    read_png_file("usa_sm.png");
     int z = 3;
 
-    int o[width][height][z];
-    for(int y = 0; y < height; y++) {
-      png_bytep row = row_pointers[y];
-      for(int x = 0; x < width; x++) {
-        png_bytep px = &(row[x * 4]);
-        //printf("%d, %d, %d\n", px[0], px[1], px[2]);
-        for(int l = 0; l < z; l++){
-          o[x][y][l] = px[l];
-        }
+    int *o = malloc(height*width*z * sizeof(int));
+    for(int i = 0; i < height; i++) {
+      png_bytep row = row_pointers[i];
+      for(int j = 0; j < width; j++) {
+        png_bytep py = &(row[j * 4]);
+        o[i*width*z+j*z+0] = py[0];
+        o[i*width*z+j*z+1] = py[1];
+        o[i*width*z+j*z+2] = py[2];
       }
     }
 
@@ -393,20 +503,22 @@ int main(void)
 
     /* Scan the image to set the density field in the states.
     In addition, calculate the average density. */
-    double u[width][height];
-    double cu[width][height];
+    //double  u[height*width];
+    double *u = malloc(height*width * sizeof(double));
+    //double cu[height*width];
+    double *cu = malloc(height*width * sizeof(double));
     double srho = 0.0;
     int npts = 0;
     int co;
     int index;
 
-    for(int y=0; y < height; y++){
-      for(int x=0; x < width; x++){
-        co = o[x][y][0]+256*o[x][y][1]+65536*o[x][y][2];
+    for(int i=0; i < height; i++){
+      for(int j=0; j < width; j++){
+        co = o[i*width*z+j*z+0]+256*o[i*width*z+j*z+1]+65536*o[i*width*z+j*z+2];
         index = getIntIndex(d, co);
         if (index != -1){
-          u[x][y] = rh[index];
-          srho+=u[x][y];
+          u[i*width+j] = rh[index];
+          srho+=u[i*width+j];
           npts+=1;
         }
         else if(co != 16777215){
@@ -419,24 +531,30 @@ int main(void)
     in regions outside the states */
     double rhobar=srho/npts;
     printf("Avg. rho: %f\n", rhobar);
-    for(int y=0; y < height; y++){
-      for(int x=0; x < width; x++){
-        co = o[x][y][0]+256*o[x][y][1]+65536*o[x][y][2];
+    for(int i=0; i < height; i++){
+      for(int j=0; j < width; j++){
+        co = o[i*width*z+j*z+0]\
+             +256*o[i*width*z+j*z+1]\
+             +65536*o[i*width*z+j*z+2];
         if(co==16777215){
-          u[x][y] = rhobar;
+          u[i*width+j] = rhobar;
         }
       }
     }
 
     /* Initialize the reference map coordinates */
-    double X[width][height][2];
-    double cX[width][height][2];
-    for(int y=0; y < height; y++){
-      for(int x=0; x < width; x++){
-        X[x][y][0] = h*x;
-        X[x][y][1] = h*y;
+    //double  X[height*width*2];
+    double *X = malloc(height*width*2 * sizeof(double));
+    //double cX[height*width*2];
+    double *cX = malloc(height*width*2 * sizeof(double));
+    for(int i=0; i < height; i++){
+      for(int j=0; j < width; j++){
+        X[i*width*2+j*2+0] = h*i;
+        X[i*width*2+j*2+1] = h*j;
       }
     }
+
+    //printf("x: %f, y: %f\n", X[100*width*2+50*2+0], X[100*width*2+50*2+1]);
 
     /* Calculate timestep size */
     double dt = 0.24*h*h;
@@ -445,6 +563,8 @@ int main(void)
     dt = T/nsteps;
     printf("Solving to T= %10f using %d timesteps.\n", T, nsteps);
 
+<<<<<<< HEAD
+=======
     /* Function to integrate the density and reference
     map fields forward in time by dt */
     //void step(double dt){
@@ -456,22 +576,64 @@ int main(void)
       /* Print the current time and the extremal values of density */
 
   //}
+>>>>>>> cf7fbfddaad2973a2a77eb47d00905ab16315722
 
     /* Perform the integration timesteps, using the smaller
     dt for the first few steps to deal with the large velocities
     that initially occur */
     double time = 0;
+<<<<<<< HEAD
+    for(int l=0; l < 24; l++){
+      step(dt/24.0, &time, u, cu, X, cX, h, ih2);
+    }
+    for(int l=1; l < nsteps;l++){
+      step(dt/24.0, &time, u, cu, X, cX, h, ih2);
+    }
+
+    //int i = 70;
+    //int j = 150;
+=======
     //for(int l=0; l < 24; l++){
     step(dt, &time, &u, &cu, &X, &cX, h, ih2, width, height);
     //}
 
     printf("\ndt:%f\t h:%f\t ih2:%f\t width:%d\t height:%d\n", dt, h, ih2, width, height);
+>>>>>>> cf7fbfddaad2973a2a77eb47d00905ab16315722
 
+    //printf("for (%d, %d) X: %f, %f\n", i, j, X[i*width*2+j*2+0], X[i*width*2+j*2+1]);
+    //printf("for (%d, %d) u: %f\n", i, j, u[i*width+j]);
 
     /* Use the deformed reference map to plot the density-equalized US map */
-    int o2[width][height][z];
-    memset(o2, 255, sizeof(o2)); //This initializes o2 to 255.
+    int i2;
+    int j2;
+    //int o2[height*width*z];
+    int *o2 = malloc(height*width*z * sizeof(int));
 
+    for (int i=0; i<height; i++) {
+        for (int j=0; j<width; j++) {
+          i2=(int) X[i*width*2+j*2+0]+0.5;
+          j2=(int) X[i*width*2+j*2+1]+0.5;
+
+          if (i2<0) {
+            i2 = 0;
+            }else if(i2 > height-1){
+            i2 = height-1;
+          }
+          if(j2 < 0){
+            j2 = 0;
+          } else if (j2>(width-1)) {
+            j2= width-1;
+          }
+          o2[i*width*z+j*z+0] = o[i2*width*z+j2*z+0];
+          o2[i*width*z+j*z+1] = o[i2*width*z+j2*z+1];
+          o2[i*width*z+j*z+2] = o[i2*width*z+j2*z+2];
+        }
+    }
+
+<<<<<<< HEAD
+    //printf("%d, %d\n", i2,j2);
+
+=======
     // @Millie: Set the values of o2 through conversion of Python code lines 150 - 158
     for (int i=0; i<width; i++) {
         for (int j=0; j<height; j++) {
@@ -492,15 +654,18 @@ int main(void)
             }
         }
     }
+>>>>>>> cf7fbfddaad2973a2a77eb47d00905ab16315722
     /*This function sets the pixel values to o2. The pixel parameters are
     global vairables which are defined at the very top. */
-    for(int y = 0; y < height; y++) {
-      png_bytep row = row_pointers[y];
-      for(int x = 0; x < width; x++) {
-        png_bytep px = &(row[x * 4]);
-        for(int l = 0; l < z; l++){
-          px[l] = o2[x][y][l];
-        }
+
+    for(int i = 0; i < height; i++) {
+      png_bytep row = row_pointers[i];
+      for(int j = 0; j < width; j++) {
+        png_bytep py = &(row[j * 4]);
+        py[0] = o2[i*width*z+j*z+0];
+        py[1] = o2[i*width*z+j*z+1];
+        py[2] = o2[i*width*z+j*z+2];
+        py[3] = 255; // set the opacity of the image to 100 percent
       }
     }
 
