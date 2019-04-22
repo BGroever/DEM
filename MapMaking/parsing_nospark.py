98c3e7a1d49b3f3dba9a03948c2eff980f98fa9e
Baptiste J Lemaire <blemaire@g.harvard.edu>
2019-04-22 19:48:31 -0400

    Map making process

--- MapMaking/parsing_nospark.py
+++ MapMaking/parsing_nospark.py
@@ -12,26 +12,26 @@ for x in data:
         print(x)
         final_list += 'Washington__DC\n'
     elif 'County' in x:
-        new_string = re.findall(r"^[\w\s'-]+(?=County)", x)[0].replace(" ","_").replace("'","_")+"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
+        new_string = re.findall(r"^[\w\s'-]+(?=County)", x)[0].replace(" "," ").replace("'"," ")+'\t'+re.findall(r"[\d]+$", x)[0] #"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
         final_list += new_string + '\n'
     elif 'Borough' in x:
-        new_string = re.findall(r"^[\w\s'-]+(?=Borough)", x)[0].replace(" ","_").replace("'","_")+"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
+        new_string = re.findall(r"^[\w\s'-]+(?=Borough)", x)[0].replace(" "," ").replace(" "," ")+'\t'+re.findall(r"[\d]+$", x)[0] #+"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
         final_list += new_string + '\n'
     elif 'Census Area' in x:
-        new_string = re.findall(r"^[\w\s'-]+(?=Census Area)", x)[0].replace(" ","_").replace("'","_")+"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
+        new_string = re.findall(r"^[\w\s'-]+(?=Census Area)", x)[0].replace(" "," ").replace(" "," ")+'\t'+re.findall(r"[\d]+$", x)[0] #+"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
         final_list += new_string + '\n'
     elif 'Municipality' in x:
-        new_string = re.findall(r"^[\w\s'-]+(?=Municipality)", x)[0].replace(" ","_").replace("'","_")+"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
+        new_string = re.findall(r"^[\w\s'-]+(?=Municipality)", x)[0].replace(" "," ").replace(" "," ")+'\t'+re.findall(r"[\d]+$", x)[0] #+"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
         final_list += new_string + '\n'
     elif 'City' in x:
-        new_string = re.findall(r"^[\w\s'-]+(?=City)", x)[0].replace(" ","_").replace("'","_")+"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
+        new_string = re.findall(r"^[\w\s'-]+(?=City)", x)[0].replace(" "," ").replace(" "," ")+'\t'+re.findall(r"[\d]+$", x)[0] #+"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
         final_list += new_string + '\n'
     elif 'Parish' in x:
-        new_string = re.findall(r"^[\w\s'-]+(?=Parish)", x)[0].replace(" ","_")+"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
+        new_string = re.findall(r"^[\w\s'-]+(?=Parish)", x)[0].replace(" "," ")+'\t'+re.findall(r"[\d]+$", x)[0] #+"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
         final_list += new_string + '\n'
     else:
         print(x)
 
 
-with open("Cty_state.txt","w") as temp_file:
+with open("CtyFIPS.txt","w") as temp_file:
     temp_file.write(final_list)
