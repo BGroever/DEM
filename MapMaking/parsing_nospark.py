5b2d0fc2a7da4345be3cb9053903763d2f0224bf
Baptiste J Lemaire <blemaire@g.harvard.edu>
2019-04-22 11:23:27 -0400

    Adding mapchart scripts

---
+++ MapMaking/parsing_nospark.py
@@ -0,0 +1,37 @@
+import re
+
+with open("FIPS_COUNTY_NOSTATE.txt","r") as temp_file:
+    data = temp_file.readlines()
+
+
+final_list = ''
+
+for x in data:
+    if x.startswith('District'):
+        print('Here')
+        print(x)
+        final_list += 'Washington__DC\n'
+    elif 'County' in x:
+        new_string = re.findall(r"^[\w\s'-]+(?=County)", x)[0].replace(" ","_").replace("'","_")+"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
+        final_list += new_string + '\n'
+    elif 'Borough' in x:
+        new_string = re.findall(r"^[\w\s'-]+(?=Borough)", x)[0].replace(" ","_").replace("'","_")+"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
+        final_list += new_string + '\n'
+    elif 'Census Area' in x:
+        new_string = re.findall(r"^[\w\s'-]+(?=Census Area)", x)[0].replace(" ","_").replace("'","_")+"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
+        final_list += new_string + '\n'
+    elif 'Municipality' in x:
+        new_string = re.findall(r"^[\w\s'-]+(?=Municipality)", x)[0].replace(" ","_").replace("'","_")+"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
+        final_list += new_string + '\n'
+    elif 'City' in x:
+        new_string = re.findall(r"^[\w\s'-]+(?=City)", x)[0].replace(" ","_").replace("'","_")+"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
+        final_list += new_string + '\n'
+    elif 'Parish' in x:
+        new_string = re.findall(r"^[\w\s'-]+(?=Parish)", x)[0].replace(" ","_")+"__"+re.findall(r"\w[\w\s]+(?=\s\d+)", x)[0].replace(" ","_")
+        final_list += new_string + '\n'
+    else:
+        print(x)
+
+
+with open("Cty_state.txt","w") as temp_file:
+    temp_file.write(final_list)
