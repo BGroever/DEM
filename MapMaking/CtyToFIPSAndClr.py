4a7a0db3dc9d89cccca955eb6ac18dc231b65360
Baptiste J Lemaire <blemaire@g.harvard.edu>
2019-04-23 10:09:45 -0400

    Design and Map Making

---
+++ MapMaking/CtyToFIPSAndClr.py
@@ -0,0 +1,104 @@
+import re
+
+with open("CtyFIPS.txt","r") as temp_file:
+    dataF = temp_file.readlines()
+
+with open("ReferenceFileWithStates.txt","r") as temp_file:
+    dataR = temp_file.readlines()
+
+with open("StateFIPS.txt", "r") as temp_file:
+    dataS = temp_file.readlines()
+
+state_fips = {}
+
+for line in dataS:
+    state, fips = line.split('\t')
+    state_fips[state]=int(fips)
+print(state_fips)
+
+FinalRef = ''
+counter1 = 0
+counter2 = 0
+for count, line in enumerate(dataF):
+    #print("Count: ", count, " line: ", line.replace("\n",""))
+    cty, fips = line.split('\t')
+    cty = cty.strip()
+    if cty == 'Saint Clair':
+        cty = 'St Clair'
+    elif cty == 'Hoonah-Angoon':
+        cty = 'Hoonah'
+    elif cty == 'Juneau City and':
+        cty = 'Juneau'
+    elif cty == 'Matanuska-Susitna':
+        cty = 'MatanuskaSusitna'
+    elif cty == 'Prince of Wales-Hyder':
+        cty = 'Prince of Wales Hyder'
+    elif cty == 'Sitka City and':
+        cty = 'Sitka'
+    elif cty == 'Southeast Fairbanks':
+        cty = 'Fairbanks'
+    elif cty == 'Valdez-Cordova':
+        cty = 'ValdezCordova'
+    elif cty == 'Wrangell City and':
+        cty = 'Wrangell'
+    elif cty == 'Yakutat City and':
+        cty = 'Yakutat'
+    elif cty == 'Yukon-Koyukuk':
+        cty = 'YukonKoyukuk'
+    elif cty == 'Saint Francis':
+        cty = 'St Francis'
+    elif cty == 'Rio Grande':
+        cty = 'Rio Grande'
+    elif cty == 'Miami-Dade':
+        cty = 'Miami'
+    elif cty == 'Saint Johns':
+        cty = 'St Johns'
+    elif cty == 'Saint Lucie':
+        cty = 'St Lucie'
+    elif cty == 'Saint Joseph':
+        cty = 'St Joseph'
+    elif cty == 'Saint Mary s':
+        cty = 'St Mary s'
+    elif cty.split()[0]=='Saint':
+        cty = 'St'+' '+cty.split()[1]
+    fips = fips.strip()
+    counter1 += 1
+    found = 0
+    for lineR in dataR:
+        ctyR, stateR, RGBR = lineR.split('\t')
+        if cty in lineR and int(float(fips)/1000)==state_fips[stateR]:
+            FinalRef += line.replace("\n","") + '\t' + re.findall(r'\(\d+,\ \d+,\ \d+\)', lineR)[0].replace(" ","") + '\n'
+            dataR.remove(lineR)
+            counter2 += 1
+            found = 1
+            break
+    if not found:
+        if cty == 'Rio Grande':
+            FinalRef += line.replace("\n","") + '\t' + '(255,247,214)\n'
+        elif cty == 'Clayton':
+            FinalRef += line.replace("\n","") + '\t' + '(255,233,98)\n'
+        elif cty == 'Greenup':
+            FinalRef += line.replace("\n","") + '\t' + '(255,218,108)\n'
+        elif cty == 'LaSalle':
+            FinalRef += line.replace("\n","") + '\t' + '(255,214,42)\n'
+        elif cty == 'Lake of the Woods':
+            FinalRef += line.replace("\n","") + '\t' + '(255,210,106)\n'
+        elif cty == 'Red Lake':
+            FinalRef += line.replace("\n","") + '\t' + '(255,210,76)\n'
+        elif cty == 'Jefferson Davis':
+            FinalRef += line.replace("\n","") + '\t' + '(255,133,248)\n'
+        elif cty == 'Scotts Bluff':
+            FinalRef += line.replace("\n","") + '\t' + '(255,191,0)\n'
+        elif cty == 'Allegany':
+            FinalRef += line.replace("\n","") + '\t' + '(255,212,24)\n'
+        elif cty == 'Saint Lawrence':
+            FinalRef += line.replace("\n","") + '\t' + '(255,133,248)\n'
+
+        else:
+            print("Not Found:", line.replace("\n",""))
+
+with open("MapReference.txt", "w") as temp_file:
+    temp_file.write(FinalRef)
+
+print("Counter1 = {} and Counter2 = {}".format(counter1, counter2))
+
--- MapMaking/parsemap.py
+++ MapMaking/parsemap.py
@@ -15,13 +15,14 @@ for count, county in enumerate(allcty):
     color -= 10
     newcolor = hex(color)[2:].zfill(6)
     script += '"#{}":{{"div":"#box{:d}","label":"","paths":[{}]}},'.format(newcolor, count+1, county)
-    reffile += '{}\t{}\n'.format(county, newcolor)
+    print(county)
+    reffile += '{}\t{}\t{}\n'.format(re.findall(r'(^.*\w(?=__\w\w)|^.*\w(?=_\w\w))', county)[0].replace('"',"").replace("_"," ").strip(), re.findall(r'[A-Z]{2}"$',county)[0].replace('"',""), tuple(int(newcolor[i:i+2], 16) for i in (0, 2, 4)))
 
 script = script[:-1]
-script += '}},"title":"","hidden":[],"background":"#ffffff","borders":""}}'
+script += '},"title":"","hidden":[],"background":"#ffffff","borders":""}'
 
 with open("FinalScript.txt", "w") as temp_file:
     temp_file.write(script)
 
-with open("ReferenceFile.txt",'w') as temp_file:
+with open("ReferenceFileWithStates.txt",'w') as temp_file:
     temp_file.write(reffile)
