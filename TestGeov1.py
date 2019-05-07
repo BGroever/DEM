30f89440640b94562555b0c77b20b1b10c970536
ben <groever@g.harvard.edu>
2019-05-07 19:18:44 -0400

    cleanup

--- TestGeov1.py
+++
@@ -1,4 +0,0 @@
-import geoplot
-
-geoplot.polyplot(df, figsize=(8, 4))
-plt.show()
--- Testv1.py
+++
@@ -1,6 +0,0 @@
-import geopandas
-import matplotlib.pyplot as plt
-
-world = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
-world.plot()
-plt.show()
