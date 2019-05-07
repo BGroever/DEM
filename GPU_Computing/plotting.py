894149e6d5d1850a3e4708155c7743029b88e33f
Baptiste J Lemaire <blemaire@g.harvard.edu>
2019-05-07 16:48:51 -0400

    Adding GPU/CPU comparison graph

--- GPU_Computing/plotting.py
+++ GPU_Computing/plotting.py
@@ -51,22 +51,32 @@ plt.savefig('SpeedupVsThreads.png')
 sizeN = [20400,82560,185760,330240,516000,743040,1320960,2064000]
 etime = [2.18476,7.498288,26.764,77.167,179.900253,365.85,1134.63615,2773.71]
 
-cpuN = [168300, 185800, 330200]
-eN = [124, 170, 537]
+cpuN = [20600, 168300, 185800, 330200]
+eN = [1.8, 124, 170, 537]
 
 
 
 lsN = [np.log(itm) for itm in sizeN]
 let = [np.log(itm) for itm in etime]
 
+lsNN = [np.log(itm) for itm in cpuN]
+letN = [np.log(itm) for itm in eN]
+
 A = np.vstack([lsN[-4:], np.ones(len(lsN[-4:]))]).T
 a,b = np.linalg.lstsq(A, let[-4:], rcond=None)[0]
+AN = np.vstack([lsNN[-4:], np.ones(len(lsNN[-4:]))]).T
+aN,bN = np.linalg.lstsq(AN, letN[-4:], rcond=None)[0]
+
 xax = np.linspace(sizeN[0]-3000,sizeN[-1]+200000,200)
 yay = [np.exp(a*np.log(itm)+b) for itm in xax]
+yayN = [np.exp(aN*np.log(itm)+bN) for itm in xax]
+
 fig, ax = plt.subplots(figsize = (8,6))
+ax.loglog(xax,yay, '-', color='#07af1a',alpha=0.8, label = r"$ax+b$, with $a\simeq 1.97$")
+ax.loglog(xax,yayN, '-', color='#4386f2',alpha=0.5)
 ax.loglog(sizeN,etime, 'o', color='#bc0505',alpha=0.7, label="Datapoints")
 ax.loglog(cpuN, eN, 'o', color = '#3e83f2', alpha=0.8, label='CPU')
-ax.loglog(xax,yay, '-', color='#07af1a',alpha=0.8, label = r"$ax+b$, with $a\simeq 1.97$")
+
 plt.xlabel("Total number of pixels",fontsize=14)
 plt.ylabel("Execution Time",fontsize=14)
 plt.legend(loc='upper left', frameon=True, fancybox=True, facecolor='white', fontsize=14)
